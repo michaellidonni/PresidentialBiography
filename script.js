4fbd9e8dc9f4a9dadd955f1a9e5588ca6b5e7235
michaellidonni <526mlidonni@frhsd.com>
2025-03-03 22:50:45 -0500

    Styled everything and added works cited, and youtube video

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 function checkAnswers() {
   // Correct answers
-  var correctAnswers = {
+  let correctAnswers = {
     q1: "1865",
     q2: "Republican",
     q3: "1921",
     q4: "Heart Attack",
   };
 
-  var score = 0;
-  var form = document.getElementById("quizForm");
+  let score = 0;
+  let form = document.getElementById("quizForm");
 
-  // Check answers for each question
   for (var i = 1; i <= 4; i++) {
-    var question = form.querySelector('input[name="q' + i + '"]:checked');
+    let question = form.querySelector('input[name="q' + i + '"]:checked');
     if (question && question.value === correctAnswers["q" + i]) {
       score++;
     }
   }
 
-  // Show result
-  var result = document.getElementById("result");
+  let result = document.getElementById("result");
   if (score === 4) {
     result.innerHTML =
       "You got all the answers right! Your score is " + score + "/4.";
   } else {
     result.innerHTML = "You got " + score + " out of 4 correct.";
   }
+}
+
+function showRandomQuote() {
+  const quotes = [
+    "I have no trouble with my enemies. I can take care of my enemies all right. But my friends, my Goddamn friends, they’re the ones who keep me walking the floor at nights!",
+    "America's present need is not heroics, but healing; not nostrums, but normalcy.",
+    "I am not fit for this office and should never have been here.",
+    "Inherent rights are from God, and the tragedies of the world originate in their attempted denial.",
+    "We must have a citizenship less concerned about what the government can do for it and more anxious about what it can do for the nation.",
+    "Our most dangerous tendency is to expect too much of government, and at the same time do for it too little.",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const quoteElement = document.getElementById("quote");
+  quoteElement.textContent = quotes[randomIndex];
 }
